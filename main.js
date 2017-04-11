c2bd8a1aa9844795ca0377557c5e031375739771
Jeremy Shaw <puremana@gmail.com>
2017-04-11 23:11:22 +1200

    First commit. Current files are from the pepper flash sample file from Electron as well as a 64bit version of pepper flash.

---
+++ main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+    app.quit();
+});
+
+let ppapi_flash_path;
+
+// Specify flash path.
+// On Windows, it might be /path/to/pepflashplayer.dll
+// On OS X, /path/to/PepperFlashPlayer.plugin
+// On Linux, /path/to/libpepflashplayer.so
+if(process.platform  == 'win32'){
+  ppapi_flash_path = path.join(__dirname, 'pepflashplayer2.dll');
+} else if (process.platform == 'linux') {
+  ppapi_flash_path = path.join(__dirname, 'libpepflashplayer.so');
+} else if (process.platform == 'darwin') {
+  ppapi_flash_path = path.join(__dirname, 'PepperFlashPlayer.plugin');
+}
+
+app.commandLine.appendSwitch('ppapi-flash-path', ppapi_flash_path);
+
+// Specify flash version, for example, v18.0.0.203
+app.commandLine.appendSwitch('ppapi-flash-version', '25.0.0.127');
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    'width': 800,
+    'height': 600,
+    'webPreferences': {'plugins': true, 'allowRunningInsecureContent': true, 'nodeIntegration': false}
+  });
+  mainWindow.loadURL('http://www.kongregate.com/games/iouRPG/idle-online-universe?acomplete=iourpg');
+});
